3ea5e7af183f6c37dc3caa5ed91ae15cf654ec2f
Frank Laszlo <flaszlo@nexcess.net>
2018-02-14 14:45:49 -0500

    more riotification

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,110 @@ window.app = (function initApp (app) {
   riot.util.misc.extend(app, riot)
 
   app.conference = {
-    date: 1533182400000
+    date: 1533214800000,
+    name: 'PHPDetroit',
+    location: {
+      city: 'Detroit',
+      state: 'Michigan',
+      country: 'USA',
+      address: '',
+      zip: ''
+    },
+    tickets: 150
   }
 
-  return app;
+  app.speakers = [
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    },
+    {
+      name: 'John Doe',
+      image: 'assets/images/speakers/speaker-1.jpg',
+      title: 'CEO - Testing, Inc',
+      social: {
+        twitter: 'https://twitter.com/FrankLaszlo',
+        linkedin: 'https://linkedin.com/in/laszlof'
+      }
+    }
+  ]
+
+  return app
 })(window.app)
