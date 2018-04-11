6654b497d5cb81212e81da549c304190346205af
Frank Laszlo <flaszlo@nexcess.net>
2018-04-11 16:37:44 -0400

    fixes

--- public/contact.php
+++ public/contact.php
@@ -3,12 +3,14 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 (new Dotenv\Dotenv(__DIR__ . '/../'))->load();
 
-$from = new SendGrid\Email($_POST['name'], $_POST['email']);
+$data = json_decode(file_get_contents('php://input'), true);
+
+$from = new SendGrid\Email($data['name'], $data['email']);
 $subject = getenv('MAIL_SUBJECT');
 $to = new SendGrid\Email(getenv('MAIL_TO_NAME'), getenv('MAIL_TO_EMAIL'));
-$content = new SendGrid\Content('text/plain', $_POST['message']);
+$content = new SendGrid\Content('text/plain', $data['message']);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 $sg = new \SendGrid(getenv('SENDGRID_API_KEY'));
 $response = $sg->client->mail()->send()->post($mail);
 
-http_response_code($response->getStatusCode());
+http_response_code($response->statusCode());
